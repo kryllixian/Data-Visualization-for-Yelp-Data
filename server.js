026f45d818e59a20d063915344500bec6e9c50ba
Dawn <wanghaodawn@cmu.edu>
2017-02-13 14:46:34 -0500

    Added hbs and server for word cloud

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const hbs = require('hbs');
 const fs = require('fs');
+const url = require('url');
 
 
 var app = express();
@@ -48,11 +49,106 @@ app.get('/statistics', (req, res) => {
 });
 
 app.get('/pittsburgh', (req, res) => {
-  res.render('pittsburgh.hbs', {
-    pageTitle: 'Pittsburgh',
-    currentYear: new Date().getFullYear()
-  });
+  var query = url.parse(req.url, true).query;
+  if (JSON.stringify(query) != '{}') {
+      var wordcloudName = query.wordcloud_name;
+      switch (wordcloudName) {
+          case 'restaurants':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Restaurants @ Pittsburgh',
+                wordcloudTitle: 'RESTAURANTS',
+                wordcloudSRC: '//cdn.tagul.com/json/e0bk9q14whez',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'shopping':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Shopping @ Pittsburgh',
+                wordcloudTitle: 'SHOPPING',
+                wordcloudSRC: '//cdn.tagul.com/json/d2o3sgc0zjs4',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'food':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Foods @ Pittsburgh',
+                wordcloudTitle: 'FOODS',
+                wordcloudSRC: '//cdn.tagul.com/json/pjljugdq8p8g',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'beauty_spas':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Beauty & Spas @ Pittsburgh',
+                wordcloudTitle: 'BEAUTY & SPAS',
+                wordcloudSRC: '//cdn.tagul.com/json/5qbyq1ttkys3',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'home_services':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Home Services @ Pittsburgh',
+                wordcloudTitle: 'HOME SERVICES',
+                wordcloudSRC: '//cdn.tagul.com/json/jvvdxbu81wa9',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'nightlife':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Nightlife @ Pittsburgh',
+                wordcloudTitle: 'Nightlife',
+                wordcloudSRC: '//cdn.tagul.com/json/roqx1mgemr7g',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'health_medical':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Health & Medical @ Pittsburgh',
+                wordcloudTitle: 'HEALTH & MEDICAL',
+                wordcloudSRC: '//cdn.tagul.com/json/t1m51ar0oa4d',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'bars':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Bars @ Pittsburgh',
+                wordcloudTitle: 'BARS',
+                wordcloudSRC: '//cdn.tagul.com/json/geprp19m8sxo',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'automotive':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Automotive @ Pittsburgh',
+                wordcloudTitle: 'AUTOMOTIVE',
+                wordcloudSRC: '//cdn.tagul.com/json/6hkzh64pystc',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+          case 'local_services':
+              res.render('wordcloud.hbs', {
+                pageTitle: 'Wordcloud of Local Services @ Pittsburgh',
+                wordcloudTitle: 'LOCAL SERVICES',
+                wordcloudSRC: '//cdn.tagul.com/json/hc4efncpgfi2',
+                currentYear: new Date().getFullYear()
+              });
+              break;
+      }
+  } else {
+      res.render('pittsburgh.hbs', {
+        pageTitle: 'Pittsburgh',
+        currentYear: new Date().getFullYear()
+      });
+  }
 });
+// app.get('/wordcloud', (req, res) => {
+//     res.render('wordcloud.hbs', {
+//       pageTitle: 'Wordcloud of Restaurants @ Pittsburgh',
+//       wordcloudTitle: 'RESTAURANTS',
+//       wordcloudSRC: '//cdn.tagul.com/json/e0bk9q14whez',
+//       currentYear: new Date().getFullYear()
+//     });
+// });
 
 
 app.get('/about', (req, res) => {
